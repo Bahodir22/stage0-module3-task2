bba9d4cc9c111524b7900e72c5d9639883724d4a
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-09-19 23:41:02 +0500

    4
    ...

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,10 +6,10 @@ public class NamingConvention {
         int variable_One = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEFIVE = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
-        int variableseven = 7;
+        int variableSeven = 7;
         int variableEight = 8;
     }
 }
